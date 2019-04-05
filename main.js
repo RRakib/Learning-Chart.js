327f593e3f0bd6296ff6481432067bea4d8839bd
Rakib Uddin <16103030@iubat.edu>
2019-04-05 11:24:37 +0600

    Adding Design To The Charts

--- main.js
+++ main.js
@@ -6,8 +6,12 @@ let allChart = new Chart(myChart, {
         labels : ["Nayan" , "Aulad" , "Imran" , "Rakib" , "Niloy"],
         datasets:[{
             label : "CGPA",
-            data : [4 , 3.9 , 3.8 , 3.4 , 3.3]
-        }]
+            borderWidth: 2,
+            color: "#E2E2E2",
+            borderColor: "gray",
+            data : [4 , 3.9 , 3.8 , 3.4 , 3.39],
+            backgroundColor: ["#ffeb3b" , "lightgreen", "#9cb8c5", "#53b4e0", "#ea729a"],
+        }],
     },
     options : {}
 })
